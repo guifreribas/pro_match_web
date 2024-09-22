b3e0ef7920e0d9519174c6cc3ece6e9be7b5e3ac
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-22 15:23:58 +0200

    Add TypeScript SDK settings, update package.json for flowbite, and enhance Tailwind configuration with new colors and fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,67 @@
 /** @type {import('tailwindcss').Config} */
+
+import flowbitePlugin from "flowbite/plugin";
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  darkMode: "light",
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#effef1",
+          100: "#dbfde0",
+          200: "#b8fac2",
+          300: "#81f494",
+          400: "#43e55d",
+          500: "#1bd039",
+          600: "#10a929",
+          700: "#108524",
+          800: "#126921",
+          900: "#11561e",
+          950: "#03300d",
+        },
+      },
+    },
+    fontFamily: {
+      body: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      sans: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
+  },
+  plugins: [],
+};
