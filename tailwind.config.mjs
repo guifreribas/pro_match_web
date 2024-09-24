41d4735697ad48bc660d6b0efcf24a2c8ff9fa72
Guifré Puig Ribas <guifreribas@gmail.com>
2024-09-24 08:54:10 +0200

    Add @tailwindcss/typography to devDependencies, update Tailwind config to include flowbite plugin for enhanced styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-import flowbitePlugin from "flowbite/plugin";
-
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
@@ -63,5 +61,5 @@ export default {
       ],
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
